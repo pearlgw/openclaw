439742951b35b39f8f32556a030fd35462dd3096
Peter Steinberger <steipete@gmail.com>
2026-09-18 20:52:08 -0700

    refactor(config): isolate value traversal from config IO

--- src/config/mutate.ts
+++ src/config/mutate.ts
@@ -61,7 +61,6 @@ import {
 import {
   containsConfigIncludeDirective,
   hashConfigRaw,
-  rejectConfigNonFiniteNumbers,
   resolveManagedRuntimeEnvBaseline,
 } from "./io.read-helpers.js";
 import { configWriteCommittedSnapshot } from "./io.types.js";
@@ -105,6 +104,7 @@ import {
 } from "./runtime-write-application.js";
 import type { ConfigFileSnapshot, OpenClawConfig } from "./types.js";
 import { validateConfigObjectWithPlugins } from "./validation.js";
+import { rejectConfigNonFiniteNumbers } from "./value-tree.js";
 import {
   captureConfigWriteLockGuard,
   markActiveConfigMutationPath,
